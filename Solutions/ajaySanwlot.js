de3a065cd95d9252e6b92a3cb579740b376d8552
sunnySavlot <sunny.savlot@gmail.com>
2024-01-29 13:40:09 +0530

    wrote function named identity

--- Solutions/ajaySanwlot.js
+++ Solutions/ajaySanwlot.js
@@ -1,3 +1,10 @@
+// identity(x) ⇒ any
+// Write a function identity that takes an argument and returns that argument
+
+function identity(x) {
+  return x;
+}
+
 module.exports = {
   identity,
   addb,
